0d67a93c09214c553247380a66d5ea99a84f8f65
shaziaghosi <shaziaghosi@gmail.com>
2026-03-08 02:05:51 +0530

    fix deployment error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" href="#">Home</a>
+          <a className="nav-link active" href="/">Home</a>
         </li>
         {/* <li className="nav-item">
           <Link className="nav-link" to="/about">{props.aboutText}</Link>
